1600fa6890cdc4ac89faddd75f59611d17803b79
anasmohamed <manas9109@gmail.com>
2025-04-16 05:33:54 +0200

    finish SearchView

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:note_ieee/constants.dart';
+import 'package:note_ieee/models/note_model.dart';
+import 'package:note_ieee/widgets/note_item.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   const SearchView({super.key});
 
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  List<NoteModel> searchResult = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +29,30 @@ class SearchView extends StatelessWidget {
                 hintText: 'Search here',
                 border: OutlineInputBorder(),
               ),
+              onChanged: (value) {
+                searchNote(value);
+                setState(() {});
+              },
             ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: searchResult.length,
+                itemBuilder: (context, index) => NoteItem(
+                  note: searchResult[index],
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
+  }
+
+  searchNote(String title) {
+    var box = Hive.box<NoteModel>(Constants.kNoteBox);
+    searchResult = box.values.where((note) {
+      return note.title.toLowerCase().contains(title);
+    }).toList();
   }
 }
